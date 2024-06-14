f9fa8739e904c7dfd7f1ec7a88c410feb6d22ef3
ASxCRO <antonio.suups@gmail.com>
2024-06-14 13:14:52 +0200

    remove unused imports

--- api/Utils/JwtAuthenticationFilter.cs
+++ api/Utils/JwtAuthenticationFilter.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Threading;
-using System.Web;
 using System.Web.Http.Filters;
 using System.Net.Http;
 using System.Web.Http;
